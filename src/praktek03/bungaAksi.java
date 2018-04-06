2ce927f9d26f89aa8428d7598ccc6b3df1909df1
bestiyusna <bestytantiyar17@gmail.com>
2018-04-06 20:41:19 +0800

    instence, buat atribut dan panggil methood pakkk

--- src/praktek03/bungaAksi.java
+++ src/praktek03/bungaAksi.java
@@ -1,6 +1,11 @@
 package praktek03;
 public class bungaAksi {
     public static void main(String[] args) {
+        bunga s = new bunga();
+        s.jenis="mawar";
+        s.warna="putih";
+        s.harga="1500";
         
+        s.cetakInfo();
     }
 }
