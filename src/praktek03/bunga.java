d251be60a313a95efd6c7079858615f1f22e49c9
akhyat <akhyatf706@gmail.com>
2018-04-06 20:17:26 +0800

    buat method cetak info

--- src/praktek03/bunga.java
+++ src/praktek03/bunga.java
@@ -5,4 +5,10 @@ public class bunga {
     String jenis;
     String warna;
     String harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t: "+jenis+"\n"+
+                "warna \t: "+warna+"\n"+
+                "harga \t: "+harga);
+    }
 }
