bcc5b8997c7007e516834b1ca84baa126bd69071
bestiyusna <bestytantiyar17@gmail.com>
2018-04-06 21:11:02 +0800

    class bunggamawar extends bunga

--- src/praktek03/bungaAksi.java
+++ src/praktek03/bungaAksi.java
@@ -8,5 +8,11 @@ public class bungaAksi {
         s.setHarga("15000");
         
         s.cetakInfo();
+        System.out.print("Jenisnya \t: ");
+        System.out.println(s.getJenis());
+        System.out.print("Warnanya \t: ");
+        System.out.println(s.getWarna());
+        System.out.print("harganya \t: ");
+        System.out.println(s.getHarga());
     }
 }
