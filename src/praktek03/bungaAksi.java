ad9f7c4cacfeb32d5623baf4e07e38b94a0e5188
akhyat <akhyatf706@gmail.com>
2018-04-06 20:26:21 +0800

    membuat class aksi

--- src/praktek03/bungaAksi.java
+++ src/praktek03/bungaAksi.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek03;
-
-/**
- *
- * @author Mamed
- */
 public class bungaAksi {
-    
+    public static void main(String[] args) {
+        
+    }
 }
